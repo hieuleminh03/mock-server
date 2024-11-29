6d30df24c026970be9d2f66cb294a1b902bd15a7
Hiếu Lê <hieu.le@zen8labs.com>
2024-11-29 08:13:11 +0700

    allow cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const accountRoutes = require('./routes/accountRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/account', accountRoutes);
